caf9d5acefb6c4cac2353bbade6134097d560d9b
Yanghyeondong <hdyang0686@naver.com>
2025-03-05 13:13:20 +0900

    ✨ 머신러닝 추천 시스템 임시 적용

---
+++ backend/GuessGuest_Backend/src/main/java/com/spring/jpa/service/CosineSimilarityService.java
@@ -0,0 +1,68 @@
+package com.spring.jpa.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CosineSimilarityService {
+    private List<List<Double>> housesData;
+    private List<Long> houseIds;
+
+    public CosineSimilarityService() {
+        this.housesData = new ArrayList<>();
+        this.houseIds = new ArrayList<>();
+    }
+
+    public void train(List<Long> houseIds, List<List<Double>> houseData) {
+        this.housesData = houseData;
+        this.houseIds = houseIds;
+    }
+
+    public List<Long> classify(List<Double> userData) {
+        List<Long> topKHouseIds = new ArrayList<>();
+        List<Double> similarities = new ArrayList<>();
+
+        for (int i = 0; i < housesData.size(); i++) {
+            List<Double> houseData = housesData.get(i);
+
+            double dotProduct = 0;
+            for (int j = 0; j < houseData.size(); j++) {
+                dotProduct += userData.get(j) * houseData.get(j);
+            }
+
+            double userMagnitude = 0;
+            for (int j = 0; j < userData.size(); j++) {
+                userMagnitude += Math.pow(userData.get(j), 2);
+            }
+            userMagnitude = Math.sqrt(userMagnitude);
+
+            double houseMagnitude = 0;
+            for (int j = 0; j < houseData.size(); j++) {
+                houseMagnitude += Math.pow(houseData.get(j), 2);
+            }
+            houseMagnitude = Math.sqrt(houseMagnitude);
+
+            double similarity = dotProduct / (userMagnitude * houseMagnitude);
+
+            similarities.add(similarity);
+            System.out.println("similarity: " + similarity);
+        }
+
+        List<Integer> sortedIndices = new ArrayList<>();
+        for (int i = 0; i < similarities.size(); i++) {
+            sortedIndices.add(i);
+        }
+
+        Collections.sort(sortedIndices, (i1, i2) -> Double.compare(similarities.get(i2), similarities.get(i1)));
+
+        for (int i = 0; i < sortedIndices.size(); i++) {
+            int sortedIndex = sortedIndices.get(i);
+            topKHouseIds.add(houseIds.get(sortedIndex));
+        }
+
+        return topKHouseIds;
+    }
+}
--- backend/GuessGuest_Backend/src/main/java/com/spring/jpa/service/HouseService.java
+++ backend/GuessGuest_Backend/src/main/java/com/spring/jpa/service/HouseService.java
@@ -1,7 +1,10 @@
 package com.spring.jpa.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import com.spring.jpa.entity.Mbti;
 import com.spring.jpa.entity.Place;
 import com.spring.jpa.entity.User;
 import com.spring.jpa.repository.HouseRepository;
+import com.spring.jpa.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,7 +25,10 @@ import lombok.RequiredArgsConstructor;
 public class HouseService {
 	
 	private final HouseRepository houseRepository;
-	
+	private final UserRepository userRepository;
+    private final CosineSimilarityService cosineSimilarity;
+    private final DataNormalizationService dataNormalization;
+    
 	// 모든 게스트하우스 조회 (사용 안함)
 	public List<House> getHouses() {
 		return houseRepository.findAll();
@@ -85,7 +92,66 @@ public class HouseService {
 	}
 	
 	//AI 추천 게스트 하우스 리스트
-//	public List<House> getHouseByAi(UserReq userReq) {
-//		
-//	}
+	public List<House> getHouseByAi(UserReq userReq) {
+		
+		User targetUser = userRepository.findById(userReq.getUserId()).get();
+		List<House> houses = houseRepository.findAll();
+		
+		List<List<Integer>> houseData = new ArrayList<>();
+
+	    for (House house : houses) {
+	        List<Integer> houseValues = new ArrayList<>();
+	        
+	        houseValues.add( 
+	        		((house.getAge20()*25)+(house.getAge30()*35)+(house.getAge40()*45))
+	        		/(house.getAge20()+house.getAge30()+house.getAge40()) 
+	        );
+	        
+	        int max = Math.max(house.getChinese(),  Math.max(house.getJapanese(), house.getKorean()));
+	        houseValues.addAll(Arrays.asList(
+        	    house.getChinese() == max ? 1 : 0,
+        	    house.getJapanese() == max ? 1 : 0,
+        	    house.getKorean() == max ? 1 : 0
+        	));
+	        
+	        houseValues.add(house.getMbtiE() > house.getMbtiI() ? 1 : -1);
+	        houseValues.add(house.getSolo() > house.getNotSolo() ? 1 : -1);
+
+	        houseData.add(houseValues);
+	    }
+	    
+	    
+		
+//        List<Long> houseIds = new ArrayList<>();
+//        houseIds.add(1L);
+//        houseIds.add(2L);
+//        houseIds.add(3L);
+//        houseIds.add(4L);
+//        houseIds.add(5L);
+//        List<List<Integer>> houseData = new ArrayList<>();
+//        houseData.add(List.of(15, 1, 1, 1, 1, 1));
+//        houseData.add(List.of(40, 0, 1, 0, 1, 0));
+//        houseData.add(List.of(13, 1, 1, 1, 1, 1));
+//        houseData.add(List.of(22, 1, 0, 1, 0, 1));
+//        houseData.add(List.of(25, 1, 0, 1, 0, 1));
+        
+        
+//        List<List<Double>> normalizedHouseData = houseData.stream()
+//        	    .map(i -> dataNormalization.logTransform(i))
+//        	 .collect(Collectors.toList());
+//        
+//        List<Integer> userData = List.of(21, 1, 0, 1, 0, 1);
+//        List<Double> normalizedUserData = dataNormalization.logTransform(userData);
+//        
+//        cosineSimilarity.train(houseIds, normalizedHouseData);
+//
+//        List<Long> recommendedHouseIds = cosineSimilarity.classify(normalizedUserData);
+//        
+//        System.out.println("추천된 집 ID: " + recommendedHouseIds);
+        
+        
+        
+        
+        return houseRepository.findByPlaceOrderByTotalUserDesc(Place.해운대);
+	}
 }
